63e625ae31c802fc7f7b1d1a69b4edfe068b6ce3
Cory Benfield <lukasaoz@gmail.com>
2015-12-27 14:47:09 +0000

    Expose CertificateFile at the top level.

--- src/certitude/__init__.py
+++ src/certitude/__init__.py
@@ -5,4 +5,4 @@ The certitude public API.
 This module exposes only the platform-specific parts of certitude.
 """
 # Currently we only support OS X.
-from .osx import certificate_string
+from .osx import certificate_string, CertificateFile
