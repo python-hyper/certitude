feb3e8675cadb4d36d2e6d5c9c464f33caffeea6
Cory Benfield <lukasaoz@gmail.com>
2016-05-12 18:31:37 +0100

    Don't set the vars so early.

--- .travis/run.sh
+++ .travis/run.sh
@@ -9,4 +9,7 @@ eval "$(pyenv init -)"
 source ~/.venv/bin/activate
 
 tox
+
+MACOSX_DEPLOYMENT_TARGET="10.7"
+PLATFORM_NAME="macosx_10_7_intel"
 python setup.py bdist_wheel --plat-name "$PLATFORM_NAME"
